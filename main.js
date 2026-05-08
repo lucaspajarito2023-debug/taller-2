6bc53b5c0d97edcfc43602f2148ee1a447d4f00e
lucaspajarito2023-debug <lucaspajarito2023@gmail.com>
2026-05-07 21:14:47 -0600

    feat: :sparkles: avances javascript
    se agrego codigo al javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Conectado correctamente");
+// input
+const inputBuscar = document.getElementById("inputBuscar");
+//boton
+const botonBuscar=document.getElementById("mi-btn");
+//contenedores
+const contenedorimage=document.getElementById("contenedorImagen")
